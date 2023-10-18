82d99ae6dc7bbf7fe69e3652baf1f64b7ef16da5
DEzhov <DEzhov@sportmaster.ru>
2023-10-18 23:26:29 +0300

    feat: markdown book generator created

---
+++ src/main/kotlin/ru/ezhov/example/gist/AppGist.kt
@@ -0,0 +1,30 @@
+package ru.ezhov.example.gist
+
+import java.io.File
+
+fun main() {
+    val file = File("C:\\Users\\DEzhov\\gists-test\\gist для обработки")
+
+    val codeStartRegEx = "^(\\[code:\\])(\\w+)(\\[:code\\])\$".toRegex()
+    val codeEndRegEx = "^\\[\\/code\\]\$".toRegex()
+
+    val systemLineSeparator = System.getProperty("line.separator")
+
+    file.walk().iterator().forEach { file ->
+        if (file.isFile) {
+            val lines = file.readLines().map { line ->
+                if (codeStartRegEx.matches(line)) {
+                    "```" + codeStartRegEx.matchEntire(line)!!.groupValues[2]
+                } else if (codeEndRegEx.matches(line)) {
+                    "```"
+                } else {
+                    line
+                }
+            }
+
+            val resultText = lines.joinToString(separator = systemLineSeparator)
+
+            file.writeText(resultText)
+        }
+    }
+}
